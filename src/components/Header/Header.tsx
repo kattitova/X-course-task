256d4d0b8e8ec2544785d3a0ca0f55a2db673404
Kateryna Tytova <titova.kat.85@gmail.com>
2024-06-27 17:12:21 +0300

    feat: add signin page styles on 320px screen width

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,17 @@
+import { useContext } from "react";
+
 import Logo from "../Logo/Logo";
 import UserPanel from "../UserPanel/UserPanel";
+import { UserContext, userContextType } from "../../context/UserContex";
+
 import "./Header.css";
 
 export default function Header() {
+    const {isLogged} = useContext(UserContext) as userContextType;
     return (
         <header>
             <Logo />
-            <UserPanel />
+            { isLogged ? <UserPanel /> : <></> }
         </header>
     );
 };
